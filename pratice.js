0b924cf65be4061d9280ea38f03b4a655522b5f5
moulikulanthaivel <moulikulanthaivel@gmail.com>
2024-02-15 02:53:46 -0800

    class

---
+++ pratice.js
@@ -0,0 +1,18 @@
+class employee{
+empId=102;
+empName='fayaz'
+empSal=87400
+acc_Bal=0;
+another_employee(){
+
+
+this.acc_Bal=this.empId
+this.empName=this.empName
+
+}
+
+}
+let a1=new employee();
+console.log(a1);
+a1.another_employee();
+console.log(a1);
